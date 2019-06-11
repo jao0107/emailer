a5855c5dcac77043400f2d827c7a895552440565
joshua.a.tabilog <tabilog.jt@pg.com>
2019-06-11 14:54:40 -0600

    added post email

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const nodemailer = require("nodemailer");
-var express = require('express')
+
+const body = require('body-parser')
 const app = express()
 const port = process.env.PORT
 const email = process.env.EMAIL
 const password = process.env.PASS
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+var express = require('express')
+app.get('/', (req, res) => res.send('Hello!'))
 
 app.listen(port,() => console.log(`Example app listening on port!`))
 
@@ -18,18 +21,37 @@ var smtpConfig = {
     }
 }; 
 var transporter = nodemailer.createTransport(smtpConfig);
-var mailOptions = {
-
+var defaultMailOptions = {
 
        from: 'joshua.a.tabilog@gmail.com', // sender address (who sends)
        to: 'tabilog.jt@pg.com', // list of receivers (who receives)
-       subject: 'joshua.a.tabilog@gmail.com', // Subject line
-       html: 'tabilog.jt@pg.com'  //attachment path with file name     
+       subject: 'i am a default mail' // Subject lineA
+
 };
 
-app.get('/start', function (req, res) {
+app.post('/sendEmail',function(req,res){
+       const optionsBody = req.body
+       var mailOptions = {
+
+              from: optionsBody.from, // sender address (who sends)
+              to:  optionsBody.to, // list of receivers (who receives)
+              subject:  optionsBody.subject, // Subject lineA
+              text: optionsBody.text,
+             
+              html: "<b>Hello world?</b>"
+       };
+       transporter.sendMail(mailOptions, function(error) {
+              if(error){
+                     res.send(error)
+              } else {
+                     res.send('email sent!' + mailOptions)
+              }
+       
+});
+
+app.get('/test', function (req, res) {
          // send mail with defined transport object
- transporter.sendMail(mailOptions, function(error) {
+ transporter.sendMail(defaultMailOptions, function(error) {
        if(error){
               res.send(error)
        } else {
