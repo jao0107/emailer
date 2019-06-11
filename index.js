4f6a6e5941c2e69f7dae289090471302bbd7a905
joshua.a.tabilog <tabilog.jt@pg.com>
2019-06-11 14:10:45 -0600

    added port 80

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const nodemailer = require("nodemailer");
 var express = require('express')
 const app = express()
-const port = 3000
+port = process.env.PORT || 80
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port,() => console.log(`Example app listening on port!`))
 
 var smtpConfig = {
     host: 'smtp.gmail.com',
